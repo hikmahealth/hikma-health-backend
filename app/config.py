54422411f15993232d21f9b4b1c093ce08582c98
MrBass <belbizri@projexia.ca>
2022-03-09 08:29:06 -0500

    Adding json config keyfile. Adjusting CloudBuild.yaml with the new Postgres cred

--- app/config.py
+++ app/config.py
@@ -11,11 +11,11 @@ if ENV in ('dev_local', 'dev_docker', 'stg'):
 
     PG_USER = 'hikma_dev'
     # PG_PASSWORD = 'password'
-    PG_PASSWORD = '96DDDDDB6425'
-    PG_DB = 'hikma_dev'
+    PG_PASSWORD = '29ewKEHEozym7EgC'
+    PG_DB = ' hikma-db'
     FLASK_DEBUG = True
     PHOTOS_STORAGE_BUCKET = 'dev-api-photos'
-    EXPORTS_STORAGE_BUCKET = 'dev-api-exports'
+    EXPORTS_STORAGE_BUCKET = 'hikma-api-exports'
     LOCAL_PHOTO_STORAGE_DIR = '/tmp/hikma_photos'
     DEFAULT_PROVIDER_ID_FOR_IMPORT = 'bd227f3d-0fbb-45c5-beed-8ce463481415'
 
