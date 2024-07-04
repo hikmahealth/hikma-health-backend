54c91e1ebaa3c8d6d72cdeb9fddde2869ec71eb5
Kevin James <54111777+iam-kevin@users.noreply.github.com>
2024-07-04 11:50:25 +0300

    change project name to `hikmahealth`

--- hikmahealth/server/helpers/web.py
+++ hikmahealth/server/helpers/web.py
@@ -4,7 +4,7 @@ from flask import Request, request
 from functools import wraps
 from typing import Set, Dict
 
-from hikma.server.utils.errors import WebError
+from hikmahealth.server.utils.errors import WebError
 
 def assert_data_has_keys(request_arg: Request, keys: Set[str], data_type='json'):
     if data_type == 'json':
