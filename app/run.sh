2ff30b33d7b041185b1adf9f78b3f6a2b50276fb
Kevin James <54111777+iam-kevin@users.noreply.github.com>
2024-07-06 15:52:54 +0300

    make project back compat with new instructions

---
+++ app/run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# This script exists for backcompatibility reasons. 
+# Avoid making any changes/improvement here, but instead use
+#
+# cd <PROJECT_ROOT_DIR>
+# ./scripts/run.sh
+
+echo -e "\e[33mThis script is deprecated. Please follow new instructions from README on how to run the project\e[0m"
+sleep 3
+
+./run_migrations.sh
+
+cd ../
+export PYTHONUNBUFFERED=TRUE
+
+case ${APP_ENV} in
+    prod)
+        # ./cloud_sql_proxy -instances=${DB_INSTANCE}=tcp:5432 -credential_file=${GOOGLE_APPLICATION_CREDENTIALS} &
+        # sleep 5 && 
+        APP_PORT=8000 python3 pywsgi.py
+        ;;
+    dev_docker)
+        gunicorn --timeout 6000 --access-logfile - --error-logfile - --log-level debug -w 1 -b 0.0.0.0:8080 app:app
+        ;;
+    stg)
+        gunicorn --timeout 6000 --access-logfile - --error-logfile - --log-level debug -w 1 -b 0.0.0.0:42069 app:app
+        ;;
+    *)
+        APP_PORT=8000 python3 pywsgi.py
+        ;;
+esac
---
+++ app/run_migrations.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd ../
+./scripts/run_migrations.sh
