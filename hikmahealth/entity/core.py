1945220956a655e678a6e26f4866a193db45ed85
Kevin James <54111777+iam-kevin@users.noreply.github.com>
2024-07-15 21:31:50 +0300

    fix up fields to standardize data types

--- hikmahealth/entity/core.py
+++ hikmahealth/entity/core.py
@@ -2,57 +2,9 @@ from __future__ import annotations
 
 import dataclasses
         
-dataentity = dataclasses.dataclass(init=False)
+dataentity = dataclasses.dataclass(init=False, kw_only=True,)
 
-# def _initializer(self, **kwargs):
-#     names = self.fields_
-#     for k, v in kwargs.items():
-#         # 'TABLE_NAME' is a reserved field
-#         if k == 'TABLE_NAME':
-#             continue
-
-#         if k in names:
-#             setattr(self, k, v)
-
-# class dataentity:
-#     def __init__(self, cls):
-#         self._class = dataclasses.make_dataclass(cls, init=False)
-
-
-#     def __call__(self, *args: dataclasses.Any, **kwds: dataclasses.Any) -> dataclasses.Any:
-#         # checks if "TABLE_NAME" is defined
-#         getattr(self._class, 'TABLE_NAME')
-
-#         self._class.__init__ = _initializer
-#         self._class.fields_ = property(lambda self: set([f.name for f in dataclasses.fields(self) if f.name != 'TABLE_NAME']))
-#         return self._class
-
-    
-    
-# def the_entity(table_name: str | None = None):
-#     def _special_init_(self, **kwargs):
-#         names = self.fields_
-#         for k, v in kwargs.items():
-#             if k == 'TABLE_NAME':
-#                 # 'TABLE_NAME' is a reserved field
-#                 continue
-
-#             if k in names:
-#                 setattr(self, k, v)
-
-#     def fields_(self):
-#         # 'TABLE_NAME' is a reserved field
-#         return set([f.name for f in dataclasses.fields(self) if f.name != 'TABLE_NAME'])
-                
-#     def decorate_class(cls):
-#          cls.__init__ = _special_init_
-#          cls.fields_ = fields_
-#          dataclasses.dataclass(init=False)(cls)
-            
-#     return decorate_class
-
-@dataclasses.dataclass(init=False)
-class Entity():
+class Entity:
     @property
     def fields_(self):
         # 'TABLE_NAME' is a reserved field
--- hikmahealth/entity/fields.py
+++ hikmahealth/entity/fields.py
@@ -1,22 +1,80 @@
-from datetime import datetime
+from datetime import datetime, timezone
 import json
+from typing import Callable, Any, Optional
 
-class _WithSetPrivateName:
+from operator import xor
+
+
+class _blankclass:
+    _instance = None
+         
+    @classmethod
+    def create(cls):
+        if _blankclass._instance is None:
+            _blankclass._instance = cls()
+
+        return _blankclass._instance
+    
+    def __repr__(self) -> str:
+        return "__BLANK__"
+    
+
+
+# This object represnts a missing value. Since `None` can also be a value, we need a
+# way to demostrate 'nothing'. This is similar to `dataclasses.MISSING``
+BLANK = _blankclass.create()
+
+class ISODateTime:
+    def __init__(self, default_factory: Callable[[Any], datetime] = BLANK):
+        self._default_factory = default_factory
+        
     def __set_name__(self, _, name):
-        self._private_name = "_" + name     
+        self._private_name = "__" + name
 
-class FromISOStringDateTime(_WithSetPrivateName):  
-    def __get__(self, obj, _=None):
-        value = getattr(obj, self._private_name, None)
-        return datetime.fromisoformat(value)
+    def default_value(self):
+        if self._default_factory is not BLANK:
+            return self._default_factory()
+        
+        return datetime.now(tz=timezone.utc)
+            
+
+    def __get__(self, obj, _):
+        value = getattr(obj, self._private_name, BLANK)
+        if value is BLANK:
+            return self.default_value()
+        
+        return value
 
     def __set__(self, obj, value: str):
-        setattr(obj, self._private_name, value)
+        setattr(obj, self._private_name, datetime.fromisoformat(value).astimezone(timezone.utc))
 
 
-class JSON(_WithSetPrivateName):
-    def __get__(self, obj, _=None):
-        output = getattr(obj, self._private_name, None)
+class JSON:
+    def __init__(self, 
+                 default_factory: Optional[Callable[[], Any]] = BLANK, 
+                 default: Optional[Any] = BLANK):
+        
+
+        assert xor(default is BLANK, default_factory is BLANK), "either default or default_factory must be defined"
+
+        self._default_value = default
+        self._default_factory = default_factory
+        
+        
+    def pull_default(self):
+        if self._default_factory is not BLANK:
+            return self._default_factory()
+        
+        return self._default_value
+            
+    def __set_name__(self, _, name):
+        self._private_name = "__" + name  
+
+    def __get__(self, obj, _):
+        output = getattr(obj, self._private_name, BLANK)
+        if output is BLANK:
+            return self.pull_default()
+            
         return json.loads(output)
 
     def __set__(self, obj, value: str | bytes):
