7c3862c275822f55302da8f17d51714762053806
Kevin James <54111777+iam-kevin@users.noreply.github.com>
2024-07-18 15:42:14 +0300

    fix: include missing endpoint /update_patient_registration_form

--- hikmahealth/entity/fields.py
+++ hikmahealth/entity/fields.py
@@ -11,44 +11,44 @@ import json
 from typing import Callable, Any, Optional
 from operator import xor
 
-class _blankclass:
+class _BLANK:
     """Singleton implementation for the help deal with the `BLANK` constant"""
     _instance = None
     @classmethod
     def create(cls):
-        if _blankclass._instance is None:
-            _blankclass._instance = cls()
+        if _BLANK._instance is None:
+            _BLANK._instance = cls()
 
-        return _blankclass._instance
-    
+        return _BLANK._instance
+
     def __repr__(self) -> str:
         return "__BLANK__"
-    
 
 
-BLANK = _blankclass.create()
+
+BLANK = _BLANK.create()
 """This object represnts a missing value. Since `None` can also be a value, we need a
 way to demostrate 'nothing'. Similar to `dataclasses.MISSING`"""
 
 class UTCDateTime:
     """Field to represent a date object that's converted from and ISO8601 string"""
-    def __init__(self, default_factory: Callable[[Any], datetime] = BLANK):
+    def __init__(self, default_factory: Callable[[], datetime] = BLANK):
         self._default_factory = default_factory
-        
+
     def __set_name__(self, _, name):
         self._private_name = "__" + name
 
     def default_value(self):
         if self._default_factory is not BLANK:
             return self._default_factory()
-        
+
         return datetime.now(tz=timezone.utc)
-            
+
     def __get__(self, obj, _):
         value = getattr(obj, self._private_name, BLANK)
         if value is BLANK:
             return self.default_value()
-        
+
         return value
 
     def __set__(self, obj, value: str | datetime):
@@ -61,38 +61,39 @@ class UTCDateTime:
                 value_to_set = datetime.fromisoformat(value)
             except ValueError :
                 raise ValueError("input must be datetime or iso-8601 valid string")
-            
+
         setattr(obj, self._private_name, value_to_set.astimezone(timezone.utc))
 
 
 class JSON:
     """Field to represent converting JSON string into friendlier objects like `dict` or `list`"""
-    def __init__(self, 
-                 default_factory: Optional[Callable[[], Any]] = BLANK, 
-                 default: Optional[Any] = BLANK):
-        
+    def __init__(self,
+                 default_factory: Callable[[], Any] | _BLANK = BLANK,
+                 default: Any | _BLANK = BLANK):
+
 
         assert xor(default is BLANK, default_factory is BLANK), "either default or default_factory must be defined"
 
         self._default_value = default
         self._default_factory = default_factory
-        
-        
+
+
     def pull_default(self):
         if self._default_factory is not BLANK:
             return self._default_factory()
-        
+
         return self._default_value
-            
+
     def __set_name__(self, _, name):
-        self._private_name = "__" + name  
+        self._private_name = "__" + name
 
     def __get__(self, obj, _):
         output = getattr(obj, self._private_name, BLANK)
         if output is BLANK:
             return self.pull_default()
-            
+
         return json.loads(output)
+
 
     def __set__(self, obj, value: str | bytes):
         setattr(obj, self._private_name, value)
--- hikmahealth/entity/helpers.py
+++ hikmahealth/entity/helpers.py
@@ -11,38 +11,37 @@ class SimpleCRUD:
         """This refers to the name of the able associated with
         the entity"""
         raise NotImplementedError(f"require {__class__.__name__}.TABLE_NAME to be defined")
-    
+
     @classmethod
     def from_id(cls, id: str):
         with db.get_connection().cursor(row_factory=class_row(cls)) as cur:
             node = cur.execute(
                 """
-                SELECT * FROM {} WHERE is_deleted = FALSE AND id = %s
+                SELECT * FROM {} WHERE is_deleted = FALSE AND id = %s;
                 """.format(cls.TABLE_NAME),
                 [id]
             ).fetchone()
 
         return node
-    
+
     @classmethod
     def get_all(cls):
         with db.get_connection().cursor(row_factory=class_row(cls)) as cur:
             node = cur.execute(
                 """
-                SELECT * FROM {} WHERE is_deleted = FALSE AND id = %s
+                SELECT * FROM {} WHERE is_deleted = FALSE;
                 """.format(cls.TABLE_NAME),
             ).fetchall()
 
         return node
-    
+
     @classmethod
     def get_many(cls, limit: int):
         with db.get_connection().cursor(row_factory=class_row(cls)) as cur:
             node = cur.execute(
                 """
-                SELECT * FROM {} WHERE is_deleted = FALSE AND id = %s
+                SELECT * FROM {} WHERE is_deleted = FALSE;
                 """.format(cls.TABLE_NAME),
             ).fetchmany(limit)
 
         return node
-    
