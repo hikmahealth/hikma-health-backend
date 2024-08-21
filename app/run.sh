e3b6867ce7181fecbd9aecb1ca4c9699d0f422ed
3210jr <ally@elsa.health>
2024-08-21 12:00:57 -0700

    fix sex character lengths, better backwards compatibility, improved render comments

--- app/run.sh
+++ app/run.sh
@@ -9,24 +9,37 @@
 echo -e "This script is deprecated. Please follow new instructions from README on how to run the project"
 sleep 3
 
-./run_migrations.sh
-
-cd ../
 export PYTHONUNBUFFERED=TRUE
 
-case ${APP_ENV} in
-    prod)
-        # ./cloud_sql_proxy -instances=${DB_INSTANCE}=tcp:5432 -credential_file=${GOOGLE_APPLICATION_CREDENTIALS} &
-        # sleep 5 && 
-        APP_PORT=8000 python3 pywsgi.py
-        ;;
-    dev_docker)
-        gunicorn --timeout 6000 --access-logfile - --error-logfile - --log-level debug -w 1 -b 0.0.0.0:8080 hikmahealth:server.server.app
-        ;;
-    stg)
-        gunicorn --timeout 6000 --access-logfile - --error-logfile - --log-level debug -w 1 -b 0.0.0.0:42069 app:app
-        ;;
-    *)
-        APP_PORT=8000 python3 pywsgi.py
-        ;;
-esac
+# Determine the correct path to run_migrations.sh
+if [ -f "./scripts/run_migrations.sh" ]; then
+    MIGRATION_SCRIPT="./scripts/run_migrations.sh"
+    ./scripts/run_migrations.sh
+    ./scripts/run.sh
+elif [ -f "./run_migrations.sh" ]; then
+    MIGRATION_SCRIPT="./run_migrations.sh"
+    cd ..
+    ./scripts/run_migrations.sh
+    ./scripts/run.sh
+else
+    echo "Error: Please run the commands from the root folder of the hikma health backend"
+    exit 1
+fi
+
+
+# case ${APP_ENV} in
+#     prod)
+#         # ./cloud_sql_proxy -instances=${DB_INSTANCE}=tcp:5432 -credential_file=${GOOGLE_APPLICATION_CREDENTIALS} &
+#         # sleep 5 && 
+#         APP_PORT=8000 python3 pywsgi.py
+#         ;;
+#     dev_docker)
+#         gunicorn --timeout 6000 --access-logfile - --error-logfile - --log-level debug -w 1 -b 0.0.0.0:8080 hikmahealth:server.server.app
+#         ;;
+#     stg)
+#         gunicorn --timeout 6000 --access-logfile - --error-logfile - --log-level debug -w 1 -b 0.0.0.0:42069 app:app
+#         ;;
+#     *)
+#         APP_PORT=8000 python3 pywsgi.py
+#         ;;
+# esac
---
+++ migrations/versions/0fa5767e5e64_increase_sex_text_length.py
@@ -0,0 +1,37 @@
+"""increase sex text length
+
+Revision ID: 0fa5767e5e64
+Revises: 19c8d4aed7fa
+Create Date: 2024-08-21 11:12:54.868256
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "0fa5767e5e64"
+down_revision = "19c8d4aed7fa"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.alter_column(
+        "patients",
+        "sex",
+        existing_type=sa.VARCHAR(length=8),
+        type_=sa.VARCHAR(length=24),
+        existing_nullable=True,
+    )
+
+
+def downgrade():
+    op.alter_column(
+        "patients",
+        "sex",
+        existing_type=sa.VARCHAR(length=24),
+        type_=sa.VARCHAR(length=8),
+        existing_nullable=True,
+    )
