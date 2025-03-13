c76df67a36894e4bcc26ddea9b8162892c3deae8
Kevin James <54111777+iam-kevin@users.noreply.github.com>
2025-03-13 20:02:08 +0300

    feat: create `Keeper`, to manage server variables

--- hikmahealth/server/test_routes.py
+++ hikmahealth/server/test_routes.py
@@ -1,6 +1,7 @@
 import logging
 import os
 from flask import Blueprint, jsonify, request
+from hikmahealth.keeper import get_keeper
 from hikmahealth.storage.client import get_storage
 
 log = logging.getLogger(__name__)
@@ -12,12 +13,29 @@ api = Blueprint('test-route', __name__)
 
 @api.route('upload', methods=['PUT'])
 def save_file_to_storage():
-	print(request.files)
-	store = get_storage()
-	for ix, (name, f) in enumerate(request.files.items()):
-		# print(f"{name}:", f)
-		dotext = os.path.splitext(f.filename)[1].lower()
-		store.put(f, f'{ix}-storage-111{dotext}', overwrite=True)
+    print(request.files)
+    store = get_storage()
+    for ix, (name, f) in enumerate(request.files.items()):
+        # print(f"{name}:", f)
+        dotext = os.path.splitext(f.filename)[1].lower()
+        store.put(f, f'{ix}-storage-111{dotext}', overwrite=True)
 
-	log.info('testing something here')
-	return jsonify(dict(ok=True)), 200
+    return jsonify(dict(ok=True)), 200
+
+
+@api.route('env', methods=['POST', 'GET'])
+def test_keeper_vals():
+    kp = get_keeper()
+    if request.method.lower() == 'post':
+        j = request.json
+        if j is not None:
+            for d in j:
+                kp.set_str(d['key'], d['value'])
+
+        return jsonify(dict(ok=True)), 201
+
+    output = dict()
+    for k in request.args.getlist('key'):
+        output[k] = kp.get(k)
+
+    return jsonify(dict(values=output)), 200
