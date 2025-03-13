b8251b1d54f6870e7ff718f4bf41ea40d3f71b51
Kevin James <54111777+iam-kevin@users.noreply.github.com>
2025-03-13 21:07:29 +0300

    update: test routes for using storage components using `Keeper`

--- hikmahealth/server/test_routes.py
+++ hikmahealth/server/test_routes.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import logging
 import os
 from flask import Blueprint, jsonify, request
@@ -27,15 +28,28 @@ def save_file_to_storage():
 def test_keeper_vals():
     kp = get_keeper()
     if request.method.lower() == 'post':
-        j = request.json
-        if j is not None:
-            for d in j:
-                kp.set_str(d['key'], d['value'])
+        l = request.get_json()
+        if l is not None:
+            for j in l:
+                d = defaultdict(lambda: None, j)
+                key = d['key']
+                if key is None:
+                    return jsonify(
+                        dict(ok=False, message=f'missing argument key in {d}')
+                    ), 400
+
+                if 'json' in d:
+                    kp.set_json(key, d.get('json'))
+                else:
+                    kp.set_str(key, d.get('value'))
 
         return jsonify(dict(ok=True)), 201
 
     output = dict()
     for k in request.args.getlist('key'):
         output[k] = kp.get(k)
+
+    for k in request.args.getlist('key_json'):
+        output[k] = kp.get_json(k)
 
     return jsonify(dict(values=output)), 200
