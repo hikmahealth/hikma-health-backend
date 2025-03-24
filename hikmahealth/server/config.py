c7219c993a54f408ed71f58060babc94413dde9c
Kevin James <54111777+iam-kevin@users.noreply.github.com>
2025-03-24 16:36:40 +0300

    fix: should get PORT from db connection uri

--- hikmahealth/server/config.py
+++ hikmahealth/server/config.py
@@ -25,12 +25,12 @@ load_dotenv('app/.env', verbose=True, override=True)
 
 
 class EnvironmentType:
-	"""Different environment expected to run in the application"""
+    """Different environment expected to run in the application"""
 
-	Prod = 'prod'
-	Staging = 'stg'
-	Local = 'dev_local'
-	Docker = 'dev_docker'
+    Prod = 'prod'
+    Staging = 'stg'
+    Local = 'dev_local'
+    Docker = 'dev_docker'
 
 
 # for PostgreSQL connection
@@ -39,37 +39,41 @@ DATABASE_URL = os.environ.get('DATABASE_URL', None)
 # This might be an azure connection string for the database
 DATABASE_URL_AZURE = os.environ.get('AZURE_POSTGRESQL_CONNECTIONSTRING', None)
 
+PG_PORT = os.environ.get('DB_PORT', '5432')
+
 # If database_url is present use it.
 if DATABASE_URL:
-	# IF there is a connection string, proceed to extract the data from it
-	db_proto, connection_params = DATABASE_URL.split('//')
-	if db_proto != 'postgresql:':
-		raise Exception('Using a non postgresql database. HH only supports PostgreSQL.')
+    # IF there is a connection string, proceed to extract the data from it
+    db_proto, connection_params = DATABASE_URL.split('//')
+    if db_proto != 'postgresql:':
+        raise Exception('Using a non postgresql database. HH only supports PostgreSQL.')
 
-	credentials, url = connection_params.split('@')
+    credentials, url = connection_params.split('@')
 
-	PG_HOST = url.split('/')[0]
-	PG_DB = url.split('/')[1]
-	PG_USER = credentials.split(':')[0]
-	PG_PASSWORD = credentials.split(':')[1]
+    host, port = url.split('/')[0].split(':')
+    PG_HOST = host
+    PG_PORT = port if port is not None else '5432'
+
+    PG_DB = url.split('/')[1]
+    PG_USER = credentials.split(':')[0]
+    PG_PASSWORD = credentials.split(':')[1]
 # if database_url_azure is present use it.
 elif DATABASE_URL_AZURE:
-	conn_str_params = {
-		pair.split('=')[0]: pair.split('=')[1] for pair in DATABASE_URL_AZURE.split(' ')
-	}
-	PG_USER = conn_str_params['user']
-	PG_PASSWORD = conn_str_params['password']
-	PG_HOST = conn_str_params['host']
-	PG_DB = conn_str_params['dbname']
-	EXPORTS_STORAGE_BUCKET = 'dev-api-exports'
-	LOCAL_PHOTO_STORAGE_DIR = '/tmp/hikma_photos'
+    conn_str_params = {
+        pair.split('=')[0]: pair.split('=')[1] for pair in DATABASE_URL_AZURE.split(' ')
+    }
+    PG_USER = conn_str_params['user']
+    PG_PASSWORD = conn_str_params['password']
+    PG_HOST = conn_str_params['host']
+    PG_DB = conn_str_params['dbname']
+    EXPORTS_STORAGE_BUCKET = 'dev-api-exports'
+    LOCAL_PHOTO_STORAGE_DIR = '/tmp/hikma_photos'
 else:
-	PG_HOST = os.environ['DB_HOST']
-	PG_DB = os.environ['DB_NAME']
-	PG_USER = os.environ['DB_USER']
-	PG_PASSWORD = os.environ['DB_PASSWORD']
+    PG_HOST = os.environ['DB_HOST']
+    PG_DB = os.environ['DB_NAME']
+    PG_USER = os.environ['DB_USER']
+    PG_PASSWORD = os.environ['DB_PASSWORD']
 
-PG_PORT = os.environ.get('DB_PORT', '5432')
 
 APP_ENV = os.environ.get('APP_ENV', EnvironmentType.Prod)
 # APP_ENV = os.environ["APP_ENV"]
@@ -78,8 +82,8 @@ FLASK_DEBUG = True
 FLASK_DEBUG_PORT = int(os.environ.get('FLASK_DEBUG_PORT', '5000'))
 
 if APP_ENV == EnvironmentType.Prod:
-	FLASK_DEBUG = False
-	FLASK_DEBUG_PORT = None
+    FLASK_DEBUG = False
+    FLASK_DEBUG_PORT = None
 
 # DEFAULT_PROVIDER_ID_FOR_IMPORT = os.environ["DEFAULT_PROVIDER_ID"]
 
