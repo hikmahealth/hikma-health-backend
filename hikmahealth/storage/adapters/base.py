11e4fc80d6f6a86a08c7d6533e05f4ae122f2be8
Kevin James <54111777+iam-kevin@users.noreply.github.com>
2025-04-03 06:14:15 +0300

    chore: resource management tasks

--- hikmahealth/storage/adapters/base.py
+++ hikmahealth/storage/adapters/base.py
@@ -28,3 +28,9 @@ class BaseAdapter:
     @abstractmethod
     def put(self, data: BytesIO, destination: str, *args, **kwargs) -> PutOutput:
         raise NotImplementedError()
+
+
+# TODO:
+# 1. make `ResourceManager` state managed by flask, so that server doesn't fail to start when variables are initially missing
+# 2. create functions to update the newly inited configurations to `Keeper`
+# 3. better the look of the /env endpoints
